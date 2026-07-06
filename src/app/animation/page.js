8030361f4bbf864f345a775d722f6aa3eab3bba5
siva doodi <doodisiva5@gmail.com>
2026-07-06 16:09:02 +0530

    06-07-2026 update

--- src/app/animation/page.js
+++ src/app/animation/page.js
@@ -1,88 +1,149 @@
-"use client"
-import { SectionHeader, VideoSection, FilterDrawer } from "../components"
+"use client";
+
+import { SectionHeader, VideoSection, FilterDrawer } from "../components";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useList } from "../hooks/useList";
 
-export default function Birds() {
-    const [openFilter, setOpenFilter] = useState(false);
-    const { data, loading, loadScrap, toast, page,
-        setPage,
-        total,
-        limit, } = useList("animation")
-    console.log(data);
-    const totalPages = Math.ceil(total / limit);
-    return (
-        <>
-            <div>
-                <SectionHeader
-                    title="Trending animation videos"
-                />
-                <VideoSection
-                    loading={loading}
-                    onScrape={loadScrap}
-                    videos={data}
-                    onFilterClick={() => setOpenFilter(true)}
-                />
-                <FilterDrawer
-                    open={openFilter}
-                    onClose={() => setOpenFilter(false)}
-                />
-                {toast.open && (
-                    <div
-                        className="fixed top-[15%] right-6 z-[9999] animate-toast-in"
-                    >
-                        <div className="rounded-xl border border-green-200 bg-white px-5 py-4 shadow-xl">
-                            <h3 className="font-semibold text-green-700">
-                                ✅ Scraping Started
-                            </h3>
-                            <p className="mt-1 text-sm text-gray-600">
-                                Videos will be updated in approximately 5 minutes.
-                            </p>
-                        </div>
-                    </div>
-                )}
+export default function Animations() {
+  const [openFilter, setOpenFilter] = useState(false);
+
+  const {
+    data,
+    loading,
+    loadScrap,
+    toast,
+    page,
+    setPage,
+    total,
+    totalPages,
+    hasNext,
+    displayVideos,
+    hasPrevious,
+    lastUpadte,
+  } = useList("animation");
+
+  const getPagination = () => {
+    const pages = [];
+    if (totalPages <= 7) {
+      return Array.from({ length: totalPages }, (_, i) => i + 1);
+    }
+    pages.push(1);
+    let start = Math.max(page - 1, 2);
+    let end = Math.min(page + 1, totalPages - 1);
+    if (page <= 3) {
+      start = 2;
+      end = 4;
+    }
+
+    if (page >= totalPages - 2) {
+      start = totalPages - 3;
+      end = totalPages - 1;
+    }
+
+    if (start > 2) {
+      pages.push("...");
+    }
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    if (end < totalPages - 1) {
+      pages.push("...");
+    }
+
+    pages.push(totalPages);
+
+    return pages;
+  };
+  return (
+    <>
+      <div>
+        <SectionHeader title="Trending Kids Videos" />
+        <VideoSection
+          loading={loading}
+          lastUpadte={lastUpadte}
+          onScrape={loadScrap}
+          refreshVideos={displayVideos}
+          videos={data}
+          total={total}
+          page={page}
+          onFilterClick={() => setOpenFilter(true)}
+        />
+        <FilterDrawer
+          open={openFilter}
+          onClose={() => setOpenFilter(false)}
+        />
+        {toast.open && (
+          <div className="fixed top-[15%] right-6 z-[9999] animate-toast-in">
+            <div className="rounded-xl border border-green-200 bg-white px-5 py-4 shadow-xl">
+              <h3 className="font-semibold text-green-700">
+                ✅ Scraping Started
+              </h3>
+
+              <p className="mt-1 text-sm text-gray-600">
+                Videos will be updated in approximately 5 minutes.
+              </p>
             </div>
-            <div className="mt-1 mb-5 flex flex-wrap items-center justify-center gap-4">
-                <div className="mt-10 flex items-center justify-center gap-3">
-                    <button
-                        disabled={page === 1}
-                        onClick={() => setPage((prev) => prev - 1)}
-                        className="cursor-pointer flex h-8 w-8 items-center justify-center rounded-full border border-border bg-white text-heading transition-all hover:border-primary hover:text-primary disabled:cursor-not-allowed disabled:opacity-40"
-                    >
-                        <ChevronLeft size={10} />
-                    </button>
+          </div>
+        )}
+      </div>
 
-                    {/* Page Numbers */}
-                    {Array.from({ length: totalPages }).map((_, index) => {
-                        const pageNumber = index + 1;
-                        return (
-                            <button
-                                key={pageNumber}
-                                onClick={() => setPage(pageNumber)}
-                                className={`flex h-8 w-8 items-center justify-center rounded-full border text-lg font-semibold transition-all duration-300  ${page === pageNumber
-                                        ? "border-primary bg-primary text-white shadow-lg shadow-primary/30 scale-105"
-                                        : "border-border bg-white text-heading hover:border-primary hover:text-primary hover:scale-105"
-                                    }`}
-                            >
-                                {pageNumber}
-                            </button>
-                        );
-                    })}
+      {/* Pagination */}
 
-                    {/* Next */}
-                    <button
-                        disabled={page === totalPages}
-                        onClick={() => setPage((prev) => prev + 1)}
-                        className="cursor-pointer flex h-8 w-8 items-center justify-center rounded-full border border-border bg-white text-heading transition-all hover:border-primary hover:text-primary disabled:cursor-not-allowed disabled:opacity-40"
-                    >
-                        <ChevronRight size={10} />
-                    </button>
+      <div className="my-10 flex justify-center">
+        <div className="flex items-center gap-2">
+          {/* Previous */}
 
-                </div>
+          <button
+            disabled={!hasPrevious}
+            onClick={() => setPage((prev) => prev - 1)}
+            className="flex h-9 w-9 items-center justify-center rounded-full border border-border bg-white hover:border-primary hover:text-primary disabled:cursor-not-allowed disabled:opacity-40"
+          >
+            <ChevronLeft size={18} />
+          </button>
 
-            </div>
+          {/* Page Numbers */}
 
-        </>
-    )
+          {getPagination().map((item, index) => {
+            if (item === "...") {
+              return (
+                <span
+                  key={`ellipsis-${index}`}
+                  className="flex h-9 w-9 items-center justify-center text-gray-500"
+                >
+                  ...
+                </span>
+              );
+            }
+
+            return (
+              <button
+                key={item}
+                onClick={() => setPage(item)}
+                className={`flex h-9 w-9 items-center justify-center rounded-full border text-sm font-semibold transition-all
+        ${page === item
+                    ? "border-primary bg-primary text-white shadow-lg shadow-primary/30"
+                    : "border-border bg-white text-heading hover:border-primary hover:text-primary"
+                  }`}
+              >
+                {item}
+              </button>
+            );
+          })}
+
+          {/* Next */}
+
+          <button
+            disabled={!hasNext}
+            onClick={() => setPage((prev) => prev + 1)}
+            className="flex h-9 w-9 items-center justify-center rounded-full border border-border bg-white hover:border-primary hover:text-primary disabled:cursor-not-allowed disabled:opacity-40"
+          >
+            <ChevronRight size={18} />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/cartoon/page.js
+++ src/app/cartoon/page.js
@@ -1,23 +1,149 @@
-'use client'
+"use client";
+
 import { SectionHeader, VideoSection, FilterDrawer } from "../components";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useState } from "react";
 import { useList } from "../hooks/useList";
 
 export default function Cartoon() {
-    const [openFilter, setOpenFilter] = useState(false);
-    const { data } = useList("cartoon")
-    return (
-        <>
-            <SectionHeader
-                title="Cartoon Videos"
-            />
-            <VideoSection
-                videos={data}
-                onFilterClick={() => setOpenFilter(true)} />
-            <FilterDrawer
-                open={openFilter}
-                onClose={() => setOpenFilter(false)}
-            />
-        </>
-    )
+  const [openFilter, setOpenFilter] = useState(false);
+
+  const {
+    data,
+    loading,
+    loadScrap,
+    toast,
+    page,
+    setPage,
+    total,
+    totalPages,
+    hasNext,
+    displayVideos,
+    hasPrevious,
+    lastUpadte,
+  } = useList("cartoon");
+
+  const getPagination = () => {
+    const pages = [];
+    if (totalPages <= 7) {
+      return Array.from({ length: totalPages }, (_, i) => i + 1);
+    }
+    pages.push(1);
+    let start = Math.max(page - 1, 2);
+    let end = Math.min(page + 1, totalPages - 1);
+    if (page <= 3) {
+      start = 2;
+      end = 4;
+    }
+
+    if (page >= totalPages - 2) {
+      start = totalPages - 3;
+      end = totalPages - 1;
+    }
+
+    if (start > 2) {
+      pages.push("...");
+    }
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    if (end < totalPages - 1) {
+      pages.push("...");
+    }
+
+    pages.push(totalPages);
+
+    return pages;
+  };
+  return (
+    <>
+      <div>
+        <SectionHeader title="Trending Kids Videos" />
+        <VideoSection
+          loading={loading}
+          lastUpadte={lastUpadte}
+          onScrape={loadScrap}
+          refreshVideos={displayVideos}
+          videos={data}
+          total={total}
+          page={page}
+          onFilterClick={() => setOpenFilter(true)}
+        />
+        <FilterDrawer
+          open={openFilter}
+          onClose={() => setOpenFilter(false)}
+        />
+        {toast.open && (
+          <div className="fixed top-[15%] right-6 z-[9999] animate-toast-in">
+            <div className="rounded-xl border border-green-200 bg-white px-5 py-4 shadow-xl">
+              <h3 className="font-semibold text-green-700">
+                ✅ Scraping Started
+              </h3>
+
+              <p className="mt-1 text-sm text-gray-600">
+                Videos will be updated in approximately 5 minutes.
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+
+      {/* Pagination */}
+
+      <div className="my-10 flex justify-center">
+        <div className="flex items-center gap-2">
+          {/* Previous */}
+
+          <button
+            disabled={!hasPrevious}
+            onClick={() => setPage((prev) => prev - 1)}
+            className="flex h-9 w-9 items-center justify-center rounded-full border border-border bg-white hover:border-primary hover:text-primary disabled:cursor-not-allowed disabled:opacity-40"
+          >
+            <ChevronLeft size={18} />
+          </button>
+
+          {/* Page Numbers */}
+
+          {getPagination().map((item, index) => {
+            if (item === "...") {
+              return (
+                <span
+                  key={`ellipsis-${index}`}
+                  className="flex h-9 w-9 items-center justify-center text-gray-500"
+                >
+                  ...
+                </span>
+              );
+            }
+
+            return (
+              <button
+                key={item}
+                onClick={() => setPage(item)}
+                className={`flex h-9 w-9 items-center justify-center rounded-full border text-sm font-semibold transition-all
+        ${page === item
+                    ? "border-primary bg-primary text-white shadow-lg shadow-primary/30"
+                    : "border-border bg-white text-heading hover:border-primary hover:text-primary"
+                  }`}
+              >
+                {item}
+              </button>
+            );
+          })}
+
+          {/* Next */}
+
+          <button
+            disabled={!hasNext}
+            onClick={() => setPage((prev) => prev + 1)}
+            className="flex h-9 w-9 items-center justify-center rounded-full border border-border bg-white hover:border-primary hover:text-primary disabled:cursor-not-allowed disabled:opacity-40"
+          >
+            <ChevronRight size={18} />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
